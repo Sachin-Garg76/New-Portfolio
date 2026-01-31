b92676919b93fb7584aaaffda5bb6360c826ad41
sachin7766 <146942718+sachin7766@users.noreply.github.com>
2026-01-31 18:59:21 +0530

    edit Experience

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -10,13 +10,18 @@ const Experience = () => {
           <Link to="/Education" className='link-tag'>Education</Link>
           <Link to="/Experience" className='link-tag'>Experience</Link>
         </div>
-        <div className="Inner-Experience-Container">
-          <div className="Entities">
-            
-          </div>
-          <div className="Entities"></div>
-          <div className="Entities"></div>
-        </div>
+        <div className="Inner-Education-Container row w-100 mt-5">
+                <div className="col-sm-6 border text-center ">
+                   <h5>Techpile pvt Ltd..</h5>
+                   <h5>ICT with infosys</h5>
+                  
+                </div>
+                <div className="col-sm-6 border text-center">
+                  <h5> MERN </h5>
+                  <h5>Pyhton with web dev</h5>
+                  
+                </div>
+              </div>
       </div>
     </>
   )
