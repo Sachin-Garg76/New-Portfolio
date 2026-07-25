69e1a0c2f4c29d1cd6346ba1740f4103c628fe21
Sachin Garg <s32314800@gmail.com>
2026-07-25 17:20:51 +0530

    Revise HomeContent layout and greeting display
    Refactor HomeContent component structure and update greeting.

--- src/Components/HomeContent.jsx
+++ src/Components/HomeContent.jsx
@@ -7,54 +7,34 @@ import { FaGithub } from "react-icons/fa6";
 import { SiLeetcode } from "react-icons/si";
 import { BsInstagram } from "react-icons/bs";
 import { FaFileDownload } from "react-icons/fa";
-import profile from "../assets/profile.png";
 
 import '../App.css'
 const HomeContent = () => {
     return (
         <>
-            <div className="HomeConent-container row">
-                <div className="Inner-Home-Container col-sm-4">
-                    <div className="Left-Inner Greeting">
-                            <div className='typewriter typewriter-greet'>
-                                <Typewriter
-                                    options={{
-                                        strings: [
-                                            ` Hello I'm`,
-                                            ` Hello I'm`,
-                                        ],
-                                        autoStart: true,
-                                        loop: true,
-                                        delay: 80,
-                                        deleteSpeed: 100,
-                                    }}
-                                />
-                            </div>
-
-                         <br />
-                        <span className="text-color typewriter-greet">
-                            Sachin
-                        </span>
-
+            <div className="HomeConent-container">
+                <div className="Hero-Center">
+                    <div className='typewriter typewriter-greet'>
+                        <Typewriter
+                            options={{
+                                strings: [
+                                    ` Hello, I'm`,
+                                    ` Hello, I'm`,
+                                ],
+                                autoStart: true,
+                                loop: true,
+                                delay: 80,
+                                deleteSpeed: 100,
+                            }}
+                        />
                     </div>
-                    <div className="Left-Inner">
-                        <div className="Links-container">
-                            <div className="Social-Links"><Link to="https://www.linkedin.com/in/sachin-garg-37aa74252/"><FaLinkedin /></Link></div>
-                            <div className="Social-Links"><Link to="https://github.com/Sachin-Garg76"><FaGithub /></Link></div>
-                            <div className="Social-Links"><Link to="https://leetcode.com/u/Sachin_323/"><SiLeetcode /></Link></div>
-                            <div className="Social-Links" ><Link to="https://www.instagram.com/sachin.1806_"><BsInstagram /></Link></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="Inner-Home-Container col-sm-4">
-                    <div className="Main-Img-Container">
-                        <img src={profile} className='Img-Main'/>
-                    </div>
-                </div>
-                <div className="Inner-Home-Container col-sm-4">
 
-                    <div className="Right-Inner Intro">
-                        <span className="text-color"> Creative <br />  </span>
+                    <span className="text-color hero-name">
+                        Sachin Garg
+                    </span>
+
+                    <div className="Intro">
+                        <span className="text-color"> Creative </span>
                         <div className='typewriter'>
                             <Typewriter
                                 options={{
@@ -70,12 +50,17 @@ const HomeContent = () => {
                             />
                         </div>
                     </div>
-                    <div className="Right-Inner">
-                        <h4 className="resume-btn">
-                            <Link className='link-tag' to="https://drive.google.com/file/d/1qjRVjdfSuTX_BNNSJEdr-YLnxmmuuuY2/view?usp=drivesdk">RESUME <FaFileDownload /></Link>
-                        </h4>
+
+                    <div className="Links-container">
+                        <div className="Social-Links"><Link to="https://www.linkedin.com/in/sachin-garg-37aa74252/"><FaLinkedin /></Link></div>
+                        <div className="Social-Links"><Link to="https://github.com/Sachin-Garg76"><FaGithub /></Link></div>
+                        <div className="Social-Links"><Link to="https://leetcode.com/u/Sachin_323/"><SiLeetcode /></Link></div>
+                        <div className="Social-Links" ><Link to="https://www.instagram.com/sachin.1806_"><BsInstagram /></Link></div>
                     </div>
 
+                    <h4 className="resume-btn">
+                        <Link className='link-tag' to="https://drive.google.com/file/d/1qjRVjdfSuTX_BNNSJEdr-YLnxmmuuuY2/view?usp=drivesdk">RESUME <FaFileDownload /></Link>
+                    </h4>
                 </div>
             </div>
 
