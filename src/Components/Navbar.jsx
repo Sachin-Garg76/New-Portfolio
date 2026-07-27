56eff2f52ffe625e8f611ba4d2027f7851cdc269
Sachin Garg <s32314800@gmail.com>
2026-07-27 13:55:01 +0530

    Adjust Navbar layout and update link structure

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,15 +7,15 @@ const Navbar = () => {
                 <div className="col-sm-3 logo">
                    Sachin
                 </div>
-                <div className="col-sm-5 ">
+                <div className="col-sm-4 ">
                 </div>
-                <div className="col-sm-4 link-container">
+                <div className="col-sm-5 link-container">
                     <Link to="/" className='link-tag' >Home</Link>
                     <Link to="/About" className='link-tag' >About</Link>
                     <Link to="/Contact" className='link-tag' >Contact</Link>
-                    <Link to="/Projects" className='link-tag' >Projects</Link>
+                    
                     <Link to="/Certificate" className='link-tag' >Certificate</Link>
-                    <Link to="/Skills" className='link-tag' >Skills</Link>
+
                 </div>
             </div>
         </>
