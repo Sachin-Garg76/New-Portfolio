a2a5b8725115a248241a4bc437858a27f6273b50
Sachin Garg <s32314800@gmail.com>
2026-07-31 12:39:31 +0530

    Implement menu toggle in Navbar component
    Added responsive menu toggle functionality to Navbar.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,35 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
 import '../styles/Navbar.css'
+
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const closeMenu = () => setMenuOpen(false);
+
     return (
         <>
             <div className="nav-container row">
                 <div className="col-sm-3 logo">
                    Sachin
                 </div>
-                <div className="col-sm-4 ">
+                <div className="col-sm-4 nav-spacer">
                 </div>
-                <div className="col-sm-5 link-container">
-                    <Link to="/" className='link-tag' >Home</Link>
-                    <Link to="/About" className='link-tag' >About</Link>
-                    <Link to="/Contact" className='link-tag' >Contact</Link>
-                    
-                    <Link to="/Certificate" className='link-tag' >Certificate</Link>
 
+                <button
+                    className="menu-toggle"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    aria-label="Toggle menu"
+                >
+                    {menuOpen ? <FaTimes /> : <FaBars />}
+                </button>
+
+                <div className={`col-sm-5 link-container ${menuOpen ? 'open' : ''}`}>
+                    <Link to="/" className='link-tag' onClick={closeMenu}>Home</Link>
+                    <Link to="/About" className='link-tag' onClick={closeMenu}>About</Link>
+                    <Link to="/Contact" className='link-tag' onClick={closeMenu}>Contact</Link>
+                    <Link to="/Certificate" className='link-tag' onClick={closeMenu}>Certificate</Link>
                 </div>
             </div>
         </>
